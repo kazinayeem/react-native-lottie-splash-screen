14bea1c7f4d5a390b99bba7fdb038b1b2cfcceb4
kazinayeem <mohammadalinayeem360@gmail.com>
2023-10-31 11:08:42 +0600

    add drawer navigation

--- App.jsx
+++ App.jsx
@@ -1,15 +1,12 @@
 import LottieSplashScreen from 'react-native-lottie-splash-screen';
 import React, {useEffect} from 'react';
-import {Button, SafeAreaView, Text} from 'react-native';
+import { Button, SafeAreaView, Text, View } from 'react-native';
 import PushNotification from 'react-native-push-notification';
-
-function App() {
-  useEffect(() => {
-    LottieSplashScreen.hide(); // here
-  }, []);
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+function HomeScreen({ navigation }) {
   const handleNotification = () => {
     PushNotification.cancelAllLocalNotifications();
-
     PushNotification.localNotification({
       channelId: 'test-channel',
       title: 'You clicked on ',
@@ -20,10 +17,39 @@ function App() {
     });
   };
   return (
-    <SafeAreaView>
-      <Text>Hello World</Text>
-      <Button title={'Click me'} onPress={handleNotification} />
-    </SafeAreaView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+      <SafeAreaView>
+        <Text>Hello World</Text>
+        <Button title={'Click me'} onPress={handleNotification} />
+      </SafeAreaView>
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+function App() {
+  useEffect(() => {
+    LottieSplashScreen.hide(); // here
+  }, []);
+  const Drawer = createDrawerNavigator();
+  return (
+    <NavigationContainer >
+      <Drawer.Navigator initialRouteName="Home" screenOptions={{ drawerActiveBackgroundColor : 'orange' }}>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import PushNotification from 'react-native-push-notification';
-
+import 'react-native-gesture-handler';
 PushNotification.configure({
   onRegister: function (token) {
     console.log('TOKEN:', token);
